e46f8427a92dc8b7951fec354ef1ed9cee98698a
Artem Gundak <artem.gundak@mail.ru>
2021-06-17 23:47:56 +0500

    chore(master): Add typography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public trigger$ = new BehaviorSubject(false)
+
   public readonly title = 'Template';
+
+  public ngOnInit(): void {
+    this.trigger$.next(true);
+  }
 }

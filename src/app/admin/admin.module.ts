777594ab3e472686a8a605f990cba773b8f6c8eb
Artem Gundak <artem.gundak@mail.ru>
2021-06-18 23:07:29 +0500

    finish sidebar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,11 +5,15 @@ import {SidebarComponent} from "./modules/sidebar/components/sidebar/sidebar.com
 import {SidebarModule} from "./modules/sidebar/sidebar.module";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {RouterModule} from "@angular/router";
+import { PacientDashboardLayoutComponent } from './components/pacient-dashboard-layout/pacient-dashboard-layout.component';
+import { PacientListLayoutComponent } from './components/pacient-list-layout/pacient-list-layout.component';
 
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
+    PacientDashboardLayoutComponent,
+    PacientListLayoutComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/components/pacient-dashboard-layout/pacient-dashboard-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pacient-dashboard-layout',
+  templateUrl: './pacient-dashboard-layout.component.html',
+  styleUrls: ['./pacient-dashboard-layout.component.scss']
+})
+export class PacientDashboardLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

cc0f14ee99de33728e3d2876689815f6be3c4f97
Artem Gundak <artem.gundak@mail.ru>
2021-06-19 03:26:03 +0500

    features

--- src/app/admin/components/patients-table/patients-table.component.ts
+++ src/app/admin/components/patients-table/patients-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../types/patient.type';
 import {PatientsHttpService} from "../../services/patients-http.service";
+import {Router, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-patients-table',
@@ -9,56 +10,19 @@ import {PatientsHttpService} from "../../services/patients-http.service";
 })
 export class PatientsTableComponent implements OnInit {
 
-  public patients: Patient[] = [
-    {
-      uid: '1',
-      age: 24,
-      email: 'sergeyrusakov1@yandex.ru',
-      firstName: 'Sergey',
-      lastName: 'Rusakov',
-      phoneNumber: '89824428864',
-    },
-    {
-      uid: '2',
-      age: 24,
-      email: 'sergeyrusakov1@yandex.ru',
-      firstName: 'Sergey',
-      lastName: 'Rusakov',
-      phoneNumber: '89824428864',
-    },
-    {
-      uid: '3',
-      age: 24,
-      email: 'sergeyrusakov1@yandex.ru',
-      firstName: 'Sergey',
-      lastName: 'Rusakov',
-      phoneNumber: '89824428864',
-    },
-    {
-      uid: '4',
-      age: 24,
-      email: 'sergeyrusakov1@yandex.ru',
-      firstName: 'Sergey',
-      lastName: 'Rusakov',
-      phoneNumber: '89824428864',
-    },
-    {
-      uid: '5',
-      age: 24,
-      email: 'sergeyrusakov1@yandex.ru',
-      firstName: 'Sergey',
-      lastName: 'Rusakov',
-      phoneNumber: '89824428864',
-    }
-  ]
+  public patients: Patient[];
 
-  constructor(private readonly patientsHttpService: PatientsHttpService) {
+  constructor(private readonly patientsHttpService: PatientsHttpService, private readonly router: Router) {
     this.patientsHttpService.getPatients().subscribe(value => {
-      console.log(value)
+      this.patients = value;
     })
   }
 
   public ngOnInit(): void {
   }
 
+  public handlePatientSelect(patient: Patient): void {
+    this.router.navigate([`admin/patients/${patient.uid}`]);
+
+  }
 }

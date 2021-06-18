bfd5db86371fcc72902259beca6d329c07f332e8
dkulagin <dkulagin@htsts.ru>
2021-06-19 00:01:27 +0500

    feat(H-5): Add user layout
    - hearth loader
    - chat component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {AdminLayoutComponent} from "./admin/components/admin-layout/admin-layout.component";
 import {PacientListLayoutComponent} from "./admin/components/pacient-list-layout/pacient-list-layout.component";
 import {PacientDashboardLayoutComponent} from "./admin/components/pacient-dashboard-layout/pacient-dashboard-layout.component";
+import {LayoutComponent} from "./user/components/layout/layout.component";
+import {ChatComponent} from "./user/components/chat/chat.component";
+import {TreatmentComponent} from "./user/components/treatment/treatment.component";
+import {PressureComponent} from "./user/components/pressure/pressure.component";
 
 const routes: Routes = [
   {
     path: 'admin', component: AdminLayoutComponent, children: [
       { path: 'patients', component: PacientListLayoutComponent },
       { path: 'patients/:id', component: PacientDashboardLayoutComponent },
+    ]
+  },
+  {
+    path: 'user', component: LayoutComponent, children: [
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {SidebarModule} from "./admin/modules/sidebar/sidebar.module";
 import {AdminModule} from "./admin/admin.module";
+import { LayoutComponent } from './user/components/layout/layout.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatIconModule} from "@angular/material/icon";
+import { ChatComponent } from './user/components/chat/chat.component';
+import { TreatmentComponent } from './user/components/treatment/treatment.component';
+import { PressureComponent } from './user/components/pressure/pressure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
+    ChatComponent,
+    TreatmentComponent,
+    PressureComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    AdminModule
+    AdminModule,
+    MatTabsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

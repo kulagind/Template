a6402ed7053697ea2b8b55222e4f01c8bf67370f
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-06-18 23:19:01 +0500

    Add basic patients table basic layout

--- src/app/admin/components/patients-table/patients-table.component.ts
+++ src/app/admin/components/patients-table/patients-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from '../../types/patient.type';
 
 @Component({
   selector: 'app-patients-table',
@@ -6,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patients-table.component.scss']
 })
 export class PatientsTableComponent implements OnInit {
+
+  public patients: Patient[] = [
+    {
+      uid: '1',
+      age: 24,
+      email: 'sergeyrusakov1@yandex.ru',
+      firstName: 'Sergey',
+      lastName: 'Rusakov',
+      phoneNumber: '89824428864',
+    },
+    {
+      uid: '1',
+      age: 24,
+      email: 'sergeyrusakov1@yandex.ru',
+      firstName: 'Sergey',
+      lastName: 'Rusakov',
+      phoneNumber: '89824428864',
+    },
+    {
+      uid: '1',
+      age: 24,
+      email: 'sergeyrusakov1@yandex.ru',
+      firstName: 'Sergey',
+      lastName: 'Rusakov',
+      phoneNumber: '89824428864',
+    },
+    {
+      uid: '1',
+      age: 24,
+      email: 'sergeyrusakov1@yandex.ru',
+      firstName: 'Sergey',
+      lastName: 'Rusakov',
+      phoneNumber: '89824428864',
+    },
+    {
+      uid: '1',
+      age: 24,
+      email: 'sergeyrusakov1@yandex.ru',
+      firstName: 'Sergey',
+      lastName: 'Rusakov',
+      phoneNumber: '89824428864',
+    }
+  ]
 
   constructor() { }
 

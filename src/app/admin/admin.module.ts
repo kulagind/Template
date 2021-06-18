e5c7669847386cc7dfa53b5a14f879b22af721d3
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-06-18 23:45:24 +0500

    Add patient delete button

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,16 @@ import { AdminLayoutComponent } from './components/admin-layout/admin-layout.com
 import { SidebarModule } from './modules/sidebar/sidebar.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
-import { PatientsListComponent } from './components/patients-list/patients-list.component';
 import { PatientsTableComponent } from './components/patients-table/patients-table.component';
 import { PacientDashboardLayoutComponent } from './components/pacient-dashboard-layout/pacient-dashboard-layout.component';
 import { PatientsListLayoutComponent } from './components/patient-list-layout/patients-list-layout.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
-    PatientsListComponent,
     PatientsTableComponent,
     PacientDashboardLayoutComponent,
     PatientsListLayoutComponent,
@@ -27,7 +27,9 @@ import { PatientsListLayoutComponent } from './components/patient-list-layout/pa
     CommonModule,
     SidebarModule,
     MatSidenavModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatIconModule
   ],
   bootstrap: [ AdminLayoutComponent ],
 

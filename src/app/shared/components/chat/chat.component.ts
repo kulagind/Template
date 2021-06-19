b4e238f2554afd454ad234a1507bf50cae80bc4c
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-06-20 04:39:25 +0500

    Add chat chat chat chat

--- src/app/shared/components/chat/chat.component.ts
+++ src/app/shared/components/chat/chat.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {ChatService} from "./chat.service";
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Chat, ChatService } from './chat.service';
 import {MESSAGES} from "../../../user/mocks/messages";
 import {AuthService} from "../../services/auth.service";
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Message } from '../../../user/interfaces/chat';
-import { switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { CookieService } from '../../cookie.service';
 
 @Component({
   selector: 'app-chat',
@@ -13,15 +14,23 @@ import { switchMap, tap } from 'rxjs/operators';
   styleUrls: ['./chat.component.scss'],
   providers: [ChatService]
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
-  public messages: Observable<Message[]>;
+  public messages: Observable<Chat>;
+
+  @ViewChild('messagesElem') messagesElem: ElementRef;
+
+  patientId: string;
+
+  public chat: Chat;
 
   constructor(
     public chatService: ChatService,
     public authService: AuthService,
     private activatedRoute: ActivatedRoute,
-  ) {}
+    private cuckoldService: CookieService,
+  ) {
+  }
 
   ngOnInit(): void {
     /**
@@ -29,18 +38,53 @@ export class ChatComponent implements OnInit {
      * */
     this.chatService.setMessages(MESSAGES);
     this.listenActivatedRoute();
+
+    this.messages = this.chatService.chats$.pipe(
+      map(chats => {
+        if (this.patientId) {
+          return chats.find(chat => chat.patientId === this.patientId);
+        } else {
+          return chats[0];
+        }
+      }),
+      tap(chat => {
+        this.chat = chat;
+        if (this.messagesElem) {
+          let div = document.getElementsByClassName("messages")[0];
+          div.scrollTo(0, div.scrollHeight);
+        }
+      }),
+    )
+
+    this.chatService.setUpdate();
+  }
+
+  ngOnDestroy(): void {
+    this.chatService.stopUpdate();
   }
 
   listenActivatedRoute(): void {
-    this.messages = this.activatedRoute.paramMap.pipe(
+    this.activatedRoute.paramMap.pipe(
       tap((params) => {
         if (this.chatService.status === 'idle') {
+          const id = params.get('patientId');
+          if (id) {
+            this.patientId = id;
+          }
           this.chatService.fetchChats(!!params.get('patientId'));
         }
-      }),
-      switchMap(params => {
-        return this.chatService.getMessages(params.get('patientId'));
-      })
-    );
+      })).subscribe();
+  }
+
+  sendMessage(content: string, element: HTMLInputElement): void {
+
+    element.value = '';
+    // @ts-ignore
+    this.chatService.postMessage(this.chat._id, {
+      author: 'Олежка',
+      // @ts-ignore
+      message: content,
+
+    });
   }
 }
--- src/app/shared/components/chat/chat.service.ts
+++ src/app/shared/components/chat/chat.service.ts
@@ -3,7 +3,9 @@ import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { Message } from '../../../user/interfaces/chat';
 import { UserService } from '../../../user/services/user.service';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+import { AdminService } from '../../../admin/services/admin.service';
+import { AdminAuthHttpService } from '../../../admin/services/admin-auth-http.service';
 
 export interface Chat {
   messages: Message[];
@@ -12,22 +14,44 @@ export interface Chat {
   id?: string;
 }
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class ChatService {
   private messages: ReplaySubject<Message[]> = new ReplaySubject<Message[]>(1);
-  private readonly chats$: BehaviorSubject<Chat[]> = new BehaviorSubject<Chat[]>([]);
+  public readonly chats$: BehaviorSubject<Chat[]> = new BehaviorSubject<Chat[]>([]);
   private userUid: string;
   private readonly doctorApi = '/api2/chat/doctor';
   private readonly patientApi = '/api2/chat/patient';
   private readonly chatApi = '/api2/chat';
   private loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private _status: 'idle' | 'fetched' = 'idle';
+  isDoctor: boolean;
+  interval: number;
+  admin: any;
 
   constructor(
     private userService: UserService,
     private http: HttpClient,
+    private adminService: AdminAuthHttpService,
   ) {
     this.userUid = userService.token;
+    this.adminService.admin$.subscribe(admin => {
+      this.admin = admin;
+    });
+
+  }
+
+  stopUpdate(): void {
+    if (this.interval) {
+      window.clearInterval(this.interval);
+    }
+  }
+
+  setUpdate(): void {
+    this.interval = window.setInterval(() => {
+      this.fetchChats(this.isDoctor);
+    }, 1000);
   }
 
   setMessages(messages: Message[]): void {
@@ -43,38 +67,54 @@ export class ChatService {
   }
 
   public postMessage(chatId: string, message: Message): void {
+    if (this.admin) {
+      message.author = this.admin.firstName + ' ' + this.admin.lastName;
+    } else {
+      message.author = 'Пользователь';
+    }
     this.http.post<Message>(`${this.chatApi}/message/${chatId}`, message)
       .subscribe((message) => {
-        this.updateWithMessage(chatId, message);
+        // @ts-ignore
+        // this.chats$.next(message);
+
+        const data = this.chats$.value;
+        // @ts-ignore
+        const index = data.findIndex(chat => chat._id === message._id);
+
+        // @ts-ignore
+        data[index] = message;
+
+        this.chats$.next(data);
       });
   }
 
   public fetchChats(isDoctor: boolean): void {
+    this.isDoctor = isDoctor;
     this.loading.next(true);
     this.http.get<Chat[]>(`${isDoctor ? this.doctorApi : this.patientApi}`)
       .subscribe(chats => {
         this.loading.next(false);
-        this.chats$.next(chats);
+        // @ts-ignore
+        this.chats$.next(isDoctor ? chats : [chats]);
         this._status = 'fetched';
       });
   }
 
-  public getMessages(patientId?: string): Observable<Message[]> {
-    return this.chats$.pipe(
-      map(chats => {
-        if (patientId) {
-          return chats.find(chat => chat.patientId === patientId)?.messages || [];
-        } else {
-          return chats[0].messages || [];
-        }
-      })
-    );
-  }
-
   private updateWithMessage(chatId: string, message: Message): void {
     const current = this.chats$.getValue();
-    current.find(chat => chat.id == chatId)?.messages.push(message);
-    this.chats$.next(current);
+    if (current?.length) {
+      current.find(chat => chat.id == chatId)?.messages.push(message);
+      this.chats$.next(current);
+    }
+  }
+
+  public createChat(chatCreateDto: Chat): void {
+    this.http.post<Chat>(this.chatApi + '/' + chatCreateDto.patientId, chatCreateDto)
+      .subscribe(chat => {
+        const data = this.chats$.value;
+        data.push(chat);
+        this.chats$.next(data);
+      });
   }
 
 }

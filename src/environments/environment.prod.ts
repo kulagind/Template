2050c6fe466b854b916a30dc09c53bf6742379e6
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2021-06-19 18:35:27 +0500

    Create example notification infrasructure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  vapidPublicKey: 'BKpp8w43Vh6Kylya57aQdcANnWJ484vWA01jYLiifL5OUIHF2AnSM2hGpDB7kbptz4X_s7A6WKSxFD0HCzv-4CI',
 };
